0a4d9eaab7d06eca0f4520fa6c145cec10f145b3
EDA <eda@mac-mini-1.local>
2016-09-05 17:01:27 +1200

    weather with photos

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ var express = require('express')
 var hbs = require('express-handlebars')
 
 var app = express()
-var blog = require('./blog')
+var weather = require('./weather')
 var path = require('path')
 
 app.engine ('hbs', hbs())
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get ('/', function (req, res) {
-  res.render('index', blog)
+  res.render('index', weather)
 })
-app.get ('/blog/:id', function (req, res) {
-  res.render('blog', blog.blog[req.params.id])
+app.get ('/weather/:id', function (req, res) {
+  res.render('weather', weather.weather[req.params.id])
 })
 
 app.listen (3000, function(){
---
+++ weather.js
@@ -0,0 +1,30 @@
+module.exports = {
+  weather:[
+    {
+      id: 0,
+      city: 'Auckland',
+      temp: '20C',
+      humidity: '70%',
+      wind_direction:'N',
+      image:'http://www3.hilton.com/resources/media/hi/AKLHIHI/en_US/img/shared/full_page_image_gallery/main/HL_viaduct_19_675x359_FitToBoxSmallDimension_Center.jpg'
+    },
+    {
+      id: 1,
+      city: 'Dogtown',
+      temp: '40C',
+      humidity: '20%',
+      wind_direction:'SW',
+      image:'https://i.ytimg.com/vi/opKg3fyqWt4/hqdefault.jpg'
+    },
+    {
+      id: 2,
+      city: 'Coffeeville',
+      temp: '30C',
+      humidity: '50%',
+      wind_direction:'SE',
+      image:'http://mw2.google.com/mw-panoramio/photos/medium/100229440.jpg'
+    },
+
+
+  ]
+}
