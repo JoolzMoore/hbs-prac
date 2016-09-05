c8ff010ebbfab6f9d022ec47813c26c47defee31
EDA <eda@mac-mini-1.local>
2016-09-05 15:00:54 +1200

    id practice

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.get ('/', function (req, res) {
   res.render('index', blog)
 })
+app.get ('/blog/:id', function (req, res) {
+  res.render('blog', blog.blog[req.params.id])
+})
 
 app.listen (3000, function(){
    console.log ('listening on port 3000')
